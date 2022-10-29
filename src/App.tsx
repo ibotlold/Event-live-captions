b679e4790f52cb20ad23cb7f5e36ee2390c7ef99
Kirill <ibotlold@gmail.com>
2022-10-29 19:54:34 +0600

    feat: changed index page content

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Web app provides live captions through Chrome's Google Cloud Speech API.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Using React as frontend.
+        </p>
       </header>
     </div>
   );
